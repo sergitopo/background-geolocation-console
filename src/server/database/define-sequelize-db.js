d73322610782cac0deb4aba611645da519d354f5
sergitopo <sergitopodeseca@gmail.com>
2020-04-02 13:52:39 +0200

    adapt to my pgsql schema

--- src/server/database/define-sequelize-db.js
+++ src/server/database/define-sequelize-db.js
@@ -1,19 +1,24 @@
 import Sequelize from 'sequelize';
 import path from 'path';
 
-import { isPostgres } from '../libs/utils';
-
 // ENVIRONMENT VARIABLES :
 // PORT (optional, defaulted to 8080) : http port server will listen to
 // DB_CONNECTION_URL (defaulted to "sqlite://db/background-geolocation.db") : connection url used to connect to a db
 //    Currently, only postgresql & sqlite dialect are supported
 //    Sample pattern for postgresql connection url : postgres://<username>:<password>@<hostname>:<port>/<dbname>
-
-export default new Sequelize(
-  isPostgres
-    ? process.env.DATABASE_URL
-    : {
-      dialect: 'sqlite',
-      storage: path.resolve(__dirname, 'db', 'background-geolocation.db'),
-    }
-);
+const database = 'xxxxxx';
+const username = 'xxxxxx';
+const password = 'xxxxxx';
+const host = 'svlpgsql10.prodevelop.es';
+const schema = 'coronatrack';
+const dialect = 'postgres'
+const config = { database, username, password };
+const options = {
+  database,
+  username,
+  password,
+  host,
+  schema,
+  dialect
+}
+export default new Sequelize(options);
